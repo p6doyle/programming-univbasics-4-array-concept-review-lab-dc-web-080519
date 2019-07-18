ad2aaea4cb3a5c32ef57fa1f7aac1de6a1e65e92
Peyton Doyle <p6doyle@gmail.com>
2019-07-18 13:48:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,6 @@
 def find_element_index(array, value_to_find)
-  counter = 0
-  
-  while array[counter] do
   array.index(value_to_find)
   return value_to_find
-  counter += 1
-  end
 end
 
 def find_max_value(array)
